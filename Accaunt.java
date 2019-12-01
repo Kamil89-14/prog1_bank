44fd3ac0e4a4f05b924bdfbc1e62cf5386901d2c
user <kamil89-14@o2.pl>
2019-12-01 11:23:28 +0100

    rachnek add

---
+++ Accaunt.java
@@ -0,0 +1,10 @@
+package com.company.bank1.bank2;
+
+import java.math.BigDecimal;
+
+public class Accaunt {
+    private String accoundNumber;
+    private AccountType accountType;
+    private BigDecimal balance;
+    
+}
