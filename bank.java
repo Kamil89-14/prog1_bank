325ba3580c345798f3e812578133d6132b09d37d
user <kamil89-14@o2.pl>
2019-12-01 10:23:42 +0100

    Dodanie folderu

---
+++ bank.java
@@ -0,0 +1,13 @@
+package com.company.bank1;
+
+public class bank {
+    private String bankName;
+
+    public bank(String bankName) {
+        this.bankName = bankName;
+
+    }
+    public String getBankName() {
+        return bankName
+    }
+}
